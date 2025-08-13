191602cdbfd80df647a35deb3f0f495e823b9aa0
vinodhkumar-12 <vinodhkumarr2018@gmail.com>
2025-08-14 03:10:18 +0530

    set operators example

--- Program.cs
+++ Program.cs
@@ -5,40 +5,55 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        int[] numbers = { 2, 2, 3, 4, 5, 6, 7, 8 };
+        int[] number1 = { 1, 1, 2, 3, 4, 5 };
+        int[] number2 = { 2, 6, 7, 8, 9 };
 
-        int[] empty_collection = { };
+        var union_result =  number1.Union(number2);
 
-        var firstValue = numbers.First();
-
-        Console.WriteLine(firstValue);
-
-        Console.WriteLine();
-
-        var lastValue = numbers.Last();
-
-        Console.WriteLine(lastValue);
-
-        Console.WriteLine();
-
-        var elementAt = numbers.ElementAt(3);
-
-        Console.WriteLine(elementAt);
-
-        Console.WriteLine();
-
-        var single = empty_collection.SingleOrDefault();
-
-        Console.WriteLine(single);
-
-        Console.WriteLine();
-
-        var default_empty =  empty_collection.DefaultIfEmpty(10);
-
-        foreach(var defaultempty in default_empty)
+        foreach(var union in union_result)
         {
-            Console.WriteLine(defaultempty);
+            Console.WriteLine(union);
         }
 
+        Console.WriteLine();
+
+
+        var intersect_result = number1.Intersect(number2);
+
+        foreach (var intersect in intersect_result)
+        {
+            Console.WriteLine(intersect);
+        }
+
+
+        Console.WriteLine();
+
+
+        var except_result = number1.Except(number2);
+
+        foreach (var except in except_result)
+        {
+            Console.WriteLine(except);
+        }
+
+        Console.WriteLine();
+
+
+        var concat_result = number1.Concat(number2);
+
+        foreach (var concat in concat_result)
+        {
+            Console.WriteLine(concat);
+        }
+
+        Console.WriteLine();
+
+
+        var distinct_result =  number1.Select(x=> new {y=x}).Distinct();
+
+        foreach (var distinct in distinct_result)
+        {
+            Console.WriteLine(distinct.y);
+        }
     }
 }
