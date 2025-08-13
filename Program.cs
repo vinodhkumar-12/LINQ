60c8c566d7febb7865fd2f38d3344883fa20dc32
vinodhkumar-12 <vinodhkumarr2018@gmail.com>
2025-08-14 01:07:40 +0530

    Aggregator operator example

--- Program.cs
+++ Program.cs
@@ -2,36 +2,23 @@
 {
     public static void Main(string[] args)
     {
-        //var employees = from employee in Employee.GetEmployees()
-        //    select employee;
+        int[] numbers = { 1, 2, 3, 4, 5, 6 };
 
-        var employees = Employee.GetEmployees().Select(x => x);
 
-        foreach (var employee in employees)
-        {
-            Console.WriteLine(employee.Id + " " + employee.FirstName + " " + employee.LastName);
-        }
+        int sumofnumbers = numbers.Sum();
 
-    }
+        int sumofEvenNumbers = numbers.Where(x => x % 2 == 0).Sum();
 
-    public class Employee
-    {
-        public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Gender { get; set; }
-        public int Age { get; set; }
-        public string DepartmentName { get; set; }
-        public double Salary { get; set; }
+        int minnumber = numbers.Min();
 
-        public static List<Employee> GetEmployees()
-        {
-            return new List<Employee>()
-            {
-                new Employee(){ Id = 1, FirstName = "Vinodh", LastName = "Kumar", Age = 28, Salary = 100000, DepartmentName = "Client", Gender = "Male"},
-                new Employee(){ Id = 2, FirstName = "Soumya", LastName = "Prakash", Age = 27, Salary = 120000, DepartmentName = "Scheduling", Gender = "Male"},
-                new Employee(){ Id = 3, FirstName = "Vijay", LastName = "Kumar", Age = 28, Salary = 100000, DepartmentName = "Billing", Gender = "Male"}
-            };
-        }
+        int maxnumber = numbers.Max();
+
+        int multiplicationofnumber = numbers.Aggregate((a,b) => a*b);
+
+        Console.WriteLine("Sum of all the numbers : {0}", sumofnumbers);
+        Console.WriteLine("Sum of all the even numbers : {0}", sumofEvenNumbers);
+        Console.WriteLine("Min number : {0}", minnumber);
+        Console.WriteLine("Max number : {0}", maxnumber);
+        Console.WriteLine("Multiplication of number : {0}", multiplicationofnumber);
     }
 }
