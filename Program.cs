98f09d07098d9e03ed1980ab3c11048cac8928b4
vinodhkumar-12 <vinodhkumarr2018@gmail.com>
2025-08-14 02:58:18 +0530

    Element operator example

--- Program.cs
+++ Program.cs
@@ -5,78 +5,40 @@ public class Program
 {
     public static void Main(string[] args)
     {
+        int[] numbers = { 2, 2, 3, 4, 5, 6, 7, 8 };
 
-        var EmployeeDepartment = Employee.GetEmployees().GroupJoin(Department.GetDepartments(), emp => emp.DepartmentId,
+        int[] empty_collection = { };
 
-                    d => d.DepartmentId, (emp, dep) => new
-                    {
-                        emp = emp,
-                        dep = dep
-                    }
+        var firstValue = numbers.First();
 
-            ).SelectMany(z=> z.dep.DefaultIfEmpty(), (a,b) => new
-            {
-                employee = a.emp,
-                DepartmentName = b == null ? "No Department" : b.DepartmentName
-            });
+        Console.WriteLine(firstValue);
 
-        //var EmployeeDepartment = from employee in Employee.GetEmployees()
-        //                         join department in Department.GetDepartments()
-        //                             on employee.DepartmentId equals department.DepartmentId into empgroup
-        //                         from emp in empgroup.DefaultIfEmpty() 
-        //                         select new
-        //                         {
-        //                             employee = employee,
-        //                             DepartmentName = emp == null? "No Department" :  emp.DepartmentName
+        Console.WriteLine();
 
-        //                         };
+        var lastValue = numbers.Last();
 
-        foreach (var emp in EmployeeDepartment)
+        Console.WriteLine(lastValue);
+
+        Console.WriteLine();
+
+        var elementAt = numbers.ElementAt(3);
+
+        Console.WriteLine(elementAt);
+
+        Console.WriteLine();
+
+        var single = empty_collection.SingleOrDefault();
+
+        Console.WriteLine(single);
+
+        Console.WriteLine();
+
+        var default_empty =  empty_collection.DefaultIfEmpty(10);
+
+        foreach(var defaultempty in default_empty)
         {
-            Console.WriteLine(emp.DepartmentName + " " + emp.employee.FirstName + " " + emp.employee.LastName);
+            Console.WriteLine(defaultempty);
         }
 
-    }
-}
-
-public class Employee
-{
-    public int Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Gender { get; set; }
-    public int Age { get; set; }
-
-    public int DepartmentId { get; set; }
-    public double Salary { get; set; }
-
-    public List<string> Skills { get; set; }
-
-    public static List<Employee> GetEmployees()
-    {
-        return new List<Employee>()
-        {
-            new Employee(){ Id = 1, FirstName = "Vinodh", LastName = "Kumar", Age = 28, Salary = 100000, DepartmentId = 1, Gender = "Male", Skills = new List<string>(){ "C#","SQL" } },
-            new Employee(){ Id = 2, FirstName = "Soumya", LastName = "Prakash", Age = 27, Salary = 120000, DepartmentId = 1, Gender = "Male", Skills = new List < string >(){ ".NET", "MVC"} },
-            new Employee(){ Id = 3, FirstName = "Vijay", LastName = "Kumar", Age = 28, Salary = 100000, DepartmentId = 2, Gender = "Male" , Skills = new List < string >() {"JAVA", "Python"} },
-            new Employee(){ Id = 4, FirstName = "Indumathi", LastName = "J", Age = 26, Salary = 80000, DepartmentId = 1, Gender = "Female" , Skills = new List < string >() {"ADO .NET", "Angular"} },
-            new Employee(){ Id = 5, FirstName = "Santhosh", LastName = "Narayan", Age = 26, Salary = 80000, Gender = "Make" , Skills = new List < string >() {"ADO .NET", "Angular"} }
-        };
-    }
-}
-
-public class Department
-{
-    public int DepartmentId { get; set; }
-    public string DepartmentName { get; set; }
-
-    public static List<Department> GetDepartments()
-    {
-        return new List<Department>()
-        {
-            new Department(){DepartmentId = 1, DepartmentName = "Scheduling"},
-            new Department(){DepartmentId = 2, DepartmentName = "Billing"},
-            new Department(){DepartmentId = 3, DepartmentName = "HR"}
-        };
     }
 }
