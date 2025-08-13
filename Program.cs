bfc5b573ea49477a5aa492c9cdd832bec30306df
vinodhkumar-12 <vinodhkumarr2018@gmail.com>
2025-08-14 00:58:44 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        var employees = from employee in Employee.GetEmployees()
+            select employee;
+
+        foreach (var employee in employees)
+        {
+            Console.WriteLine(employee.Id + " " + employee.FirstName + " " + employee.LastName);
+        }
+            
+    }
+
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+        public string DepartmentName { get; set; }
+        public double Salary { get; set; }
+
+        public static List<Employee> GetEmployees()
+        {
+            return new List<Employee>()
+            {
+                new Employee(){ Id = 1, FirstName = "Vinodh", LastName = "Kumar", Age = 28, Salary = 100000, DepartmentName = "Client", Gender = "Male"},
+                new Employee(){ Id = 2, FirstName = "Soumya", LastName = "Prakash", Age = 27, Salary = 120000, DepartmentName = "Scheduling", Gender = "Male"},
+                new Employee(){ Id = 3, FirstName = "Vijay", LastName = "Kumar", Age = 28, Salary = 100000, DepartmentName = "Billing", Gender = "Male"}
+            };
+        }
+    }
+}
