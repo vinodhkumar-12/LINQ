86bfe22affe77e353eaa7f8a49466d6d0de0498d
vinodhkumar-12 <vinodhkumarr2018@gmail.com>
2025-08-14 01:57:32 +0530

    Group By single column example

--- Program.cs
+++ Program.cs
@@ -1,21 +1,29 @@
 ﻿using static Program;
+using System.Linq;
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        //var EmployeeDescOrderByName = from employee in Employee.GetEmployees()
-        //                            orderby employee.FirstName descending, employee.LastName descending 
-        //                          select employee;
 
+        var employeeGroupByGender = from employee in Employee.GetEmployees()
+                                    group employee by employee.Gender into gender
+                                    select new
+                                    {
+                                        Gender = gender.Key,
+                                        GenderCount = gender.Count()
+                                    };
 
-        var EmployeeDescOrderByName = Employee.GetEmployees().OrderByDescending(x => x.FirstName).ThenByDescending(x => x.LastName);
-
-        foreach(var employee in EmployeeDescOrderByName)
+       //var employeeGroupByGender = Employee.GetEmployees().GroupBy(x => x.Gender).Select(x => new
+       // {
+       //     Gender = x.Key,
+       //     GenderCount = x.Count()
+       // });
+         
+        foreach(var employeeGroup in employeeGroupByGender)
         {
-            Console.WriteLine(employee.FirstName + " " + employee.LastName);
+            Console.WriteLine(employeeGroup.Gender + " " + employeeGroup.GenderCount);
         }
-
 
     }
 
@@ -37,7 +45,8 @@ public class Program
             {
                 new Employee(){ Id = 1, FirstName = "Vinodh", LastName = "Kumar", Age = 28, Salary = 100000, DepartmentName = "Client", Gender = "Male", Skills = new List<string>(){ "C#","SQL" } },
                 new Employee(){ Id = 2, FirstName = "Soumya", LastName = "Prakash", Age = 27, Salary = 120000, DepartmentName = "Scheduling", Gender = "Male", Skills = new List < string >(){ ".NET", "MVC"} },
-                new Employee(){ Id = 3, FirstName = "Vijay", LastName = "Kumar", Age = 28, Salary = 100000, DepartmentName = "Billing", Gender = "Male" , Skills = new List < string >() {"JAVA", "Python"} }
+                new Employee(){ Id = 3, FirstName = "Vijay", LastName = "Kumar", Age = 28, Salary = 100000, DepartmentName = "Billing", Gender = "Male" , Skills = new List < string >() {"JAVA", "Python"} },
+                new Employee(){ Id = 4, FirstName = "Indumathi", LastName = "J", Age = 26, Salary = 80000, DepartmentName = "Clinician", Gender = "Female" , Skills = new List < string >() {"ADO .NET", "Angular"} }
             };
         }
     }
