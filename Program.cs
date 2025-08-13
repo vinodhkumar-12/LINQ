0341fe9975f3cbac7c9efbda22e75bbad5b3b17c
vinodhkumar-12 <vinodhkumarr2018@gmail.com>
2025-08-14 01:26:57 +0530

    Select Project Example

--- Program.cs
+++ Program.cs
@@ -8,19 +8,27 @@ public class Program
         //                select new
         //                {
         //                    Id = employee.Id,
-        //                    FullName = employee.FirstName + " " + employee.LastName
+        //                    Skills = employee.Skills,
+        //                    FirstName = employee.FirstName + " " + employee.LastName   
         //                };
 
-        var employees = Employee.GetEmployees()
-                            .Select(employee => new
-                            {
-                                Id = employee.Id,
-                                FullName = employee.FirstName + " " + employee.LastName
-                            });
+
+        var employees = Employee.GetEmployees().Select(employee => new
+        {
+            Id = employee.Id,
+            Skills = employee.Skills,
+            FirstName = employee.FirstName + " " + employee.LastName
+
+        });
 
         foreach (var employee in employees)
         {
-            Console.WriteLine(employee.Id + " " + employee.FullName);
+            Console.WriteLine("Employee Name is {0}", employee.FirstName);
+
+            foreach (var skill in employee.Skills)
+            {
+                Console.WriteLine("* {0}", skill);
+            }
         }
     }
 
@@ -34,14 +42,16 @@ public class Program
         public string DepartmentName { get; set; }
         public double Salary { get; set; }
 
+        public List<string> Skills { get; set; }
+
         public static List<Employee> GetEmployees()
         {
             return new List<Employee>()
-        {
-            new Employee(){ Id = 1, FirstName = "Vinodh", LastName = "Kumar", Age = 28, Salary = 100000, DepartmentName = "Client", Gender = "Male"},
-            new Employee(){ Id = 2, FirstName = "Soumya", LastName = "Prakash", Age = 27, Salary = 120000, DepartmentName = "Scheduling", Gender = "Male"},
-            new Employee(){ Id = 3, FirstName = "Vijay", LastName = "Kumar", Age = 28, Salary = 100000, DepartmentName = "Billing", Gender = "Male"}
-        };
+            {
+                new Employee(){ Id = 1, FirstName = "Vinodh", LastName = "Kumar", Age = 28, Salary = 100000, DepartmentName = "Client", Gender = "Male", Skills = new List<string>(){ "C#","SQL" } },
+                new Employee(){ Id = 2, FirstName = "Soumya", LastName = "Prakash", Age = 27, Salary = 120000, DepartmentName = "Scheduling", Gender = "Male", Skills = new List < string >(){ ".NET", "MVC"} },
+                new Employee(){ Id = 3, FirstName = "Vijay", LastName = "Kumar", Age = 28, Salary = 100000, DepartmentName = "Billing", Gender = "Male" , Skills = new List < string >() {"JAVA", "Python"} }
+            };
         }
     }
 }
